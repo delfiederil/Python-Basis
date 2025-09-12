cf4d7105080210e43d011540e978b3bead6fe37b
Delfie Deril <delfiederil30@gmail.com>
2025-09-12 22:14:19 +0530

    Update multiplication_table.py

--- Week1_Python_Basis/multiplication_table.py
+++ Week1_Python_Basis/multiplication_table.py
@@ -1,1 +1,8 @@
+def multiplication_table():
+    num = int(input("Enter a number: "))
+    print(f"\n📊 Multiplication Table of {num}")
+    print("----------------------------")
+    for i in range(1, 11):
+        print(f"{num} x {i} = {num * i}")
 
+multiplication_table()
