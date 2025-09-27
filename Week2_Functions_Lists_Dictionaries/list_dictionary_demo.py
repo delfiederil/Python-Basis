abfb751c0ae5471a2f01f81c507447c1f2275a09
Delfie Deril <delfiederil30@gmail.com>
2025-09-27 22:55:58 +0530

    Create list_dictionary_demo.py

---
+++ Week2_Functions_Lists_Dictionaries/list_dictionary_demo.py
@@ -0,0 +1,7 @@
+# Working with lists
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])  # banana
+
+# Working with dictionaries
+person = {"name": "Alice", "age": 21, "city": "Chennai"}
+print(person["city"])
